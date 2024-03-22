fe16f9df74853800968f23b35dd936c217747317
Priyadharshini Murugan <priyadharshini.murugan@relevantz.com>
2024-03-22 21:02:22 +0530

    Add project files.

---
+++ Backend/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
+        public DbSet<Appointment> Appointments { get; set; } 
+
+        public DbSet<Service> Services { get; set; }
+
+        public DbSet<Client> Clients { get; set; }  
+    }
+}
---
+++ Backend/Models/Appointment.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    public class Appointment
+    {
+        [Key]
+        public int AppointmentId { get; set; }
+
+        public string? Name { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public TimeOnly Time { get; set; }
+
+        public long PhoneNumber { get; set; }
+
+        public string? Allergyspecification { get; set; }
+
+        public string? ClientAge { get; set; }
+
+        public int ClientId { get; set; }
+
+        [ForeignKey("ClientId")]
+        public Client? Client {  get; set; }
+
+
+        public int ServiceId { get; set; }
+
+        [ForeignKey("ServiceId")]
+        public Service? Service { get; set; }
+
+      
+
+
+
+
+    }
+}
